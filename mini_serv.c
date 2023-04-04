3edd665b455dc81584f7514b6613521a03e8150e
Stone <stone.dummett@gmail.com>
2023-04-04 11:43:00 +0200

    The version used at the exam

--- mini_serv.c
+++ mini_serv.c
@@ -8,19 +8,107 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// clang -Wall -Wextra -Werror mini_serv.c
+// ./a.out 8081
+
 typedef struct s_client {
 	int id;
 	int fd;
-	char *msg;
+	char *buf;
 	struct s_client *next;
 } t_client;
 
-fd_set current, read_set, write_set;
 t_client *clients = NULL;
+fd_set readset, writeset, current;
 int sockfd, g_id;
-char giga_buf[42 * 4096 * 42];
+char gigabuf[42 * 4096 * 42] = {0};
 
-int extract_message(char **buf, char **msg) {
+void fatal() {
+	write(2, "Fatal error\n", strlen("Fatal error\n"));
+	exit(1);
+}
+
+void send_to_all(int fd) {
+	t_client *tmp = clients;
+
+	while (tmp) {
+		if (tmp->fd != fd && FD_ISSET(tmp->fd, &writeset))
+			send(tmp->fd, gigabuf, strlen(gigabuf), 0);
+		tmp = tmp->next;
+	}
+	bzero(gigabuf, 42 * 4096 * 42);
+}
+
+void add_client() {
+	t_client *tmp = clients;
+	t_client *new = NULL;
+	struct sockaddr_in cli;
+	socklen_t len = sizeof(cli);
+
+	int connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
+	if (connfd == -1)
+		fatal();
+	new = malloc(sizeof(t_client));
+	if (!new)
+		fatal();
+	new->fd = connfd;
+	new->id = g_id++;
+	new->buf = NULL;
+	new->next = NULL;
+	FD_SET(new->fd, &current);
+	sprintf(gigabuf, "server: client %d just arrived\n", new->id);
+	send_to_all(new->fd);
+	if (clients == NULL) {
+		clients = new;
+		return;
+	}
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+}
+
+void remove_client(int fd) {
+	t_client *tmp = clients;
+	t_client *to_del = NULL;
+
+	if (clients && clients->fd == fd) {
+		to_del = clients;
+		clients = clients->next;
+	}
+	else {
+		while (tmp && tmp->next) {
+			if (tmp->next->fd == fd) {
+				to_del = tmp->next;
+				tmp->next = tmp->next->next;
+				break;
+			}
+			tmp = tmp->next;
+		}
+	}
+	if (to_del) {
+		sprintf(gigabuf, "server: client %d just left\n", to_del->id);
+		send_to_all(to_del->fd);
+		free(to_del->buf);
+		free(to_del);
+	}
+	FD_CLR(fd, &current);
+	close(fd);
+}
+
+int get_max_fd() {
+	int max_fd = sockfd;
+	t_client *tmp = clients;
+
+	while (tmp) {
+		if (tmp->fd > max_fd)
+			max_fd = tmp->fd;
+		tmp = tmp->next;
+	}
+	return max_fd;
+}
+
+int extract_message(char **buf, char **msg)
+{
 	char	*newbuf;
 	int	i;
 
@@ -28,8 +116,10 @@ int extract_message(char **buf, char **msg) {
 	if (*buf == 0)
 		return (0);
 	i = 0;
-	while ((*buf)[i]) {
-		if ((*buf)[i] == '\n') {
+	while ((*buf)[i])
+	{
+		if ((*buf)[i] == '\n')
+		{
 			newbuf = calloc(1, sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
 			if (newbuf == 0)
 				return (-1);
@@ -44,7 +134,8 @@ int extract_message(char **buf, char **msg) {
 	return (0);
 }
 
-char *str_join(char *buf, char *add) {
+char *str_join(char *buf, char *add)
+{
 	char	*newbuf;
 	int		len;
 
@@ -63,99 +154,6 @@ char *str_join(char *buf, char *add) {
 	return (newbuf);
 }
 
-int get_max_fd() {
-	t_client *tmp = clients;
-	int max_fd = sockfd;
-
-	while (tmp) {
-		if (tmp->fd > max_fd)
-			max_fd = tmp->fd;
-		tmp = tmp->next;
-	}
-	return max_fd;
-}
-
-int get_id(int fd) {
-	t_client *tmp = clients;
-
-	while (tmp) {
-		if (tmp->fd == fd)
-			return tmp->id;
-		tmp = tmp->next;
-	}
-	return 42;
-}
-
-void fatal() {
-	write(2, "Fatal error\n", strlen("Fatal error\n"));
-	close(sockfd);
-	exit(1);
-}
-
-void send_to_all(int fd) {
-	t_client *tmp = clients;
-
-	while (tmp) {
-		if (FD_ISSET(tmp->fd, &write_set) && tmp->fd != fd)
-			if (send(tmp->fd, giga_buf, strlen(giga_buf), 0) < 0)
-				fatal();
-		tmp = tmp->next;
-	}
-	bzero(giga_buf, 42 * 4096 * 42);
-}
-
-void add_client() {
-	t_client			*tmp = clients;
-	struct sockaddr_in	clientaddr;
-	socklen_t			len = sizeof(clientaddr);
-
-	int client = accept(sockfd, (struct sockaddr *)&clientaddr, (socklen_t *)&len);
-	if (client == -1)
-		fatal();
-
-	sprintf(giga_buf, "server: client %d just arived\n", g_id);
-	send_to_all(client);
-	FD_SET(client, &current);
-	t_client *new = malloc(sizeof(t_client));
-	if (!new)
-		fatal();
-	new->fd = client;
-	new->id = g_id++;
-	new->msg = NULL;
-	new->next = NULL;
-	if (!tmp)
-		clients = new;
-	else {
-		while (tmp->next)
-			tmp = tmp->next;
-		tmp->next = new;
-	}
-}
-
-void rm_client(int fd) {
-	t_client	*tmp = clients;
-	t_client	*to_del = NULL;
-
-	if (clients && clients->fd  == fd) {
-		to_del = clients;
-		clients = clients->next;
-	}
-	else {
-		while (tmp && tmp->next && tmp->next->fd != fd)
-			tmp = tmp->next;
-		if (tmp && tmp->next && tmp->next->fd == fd) {
-			to_del = tmp->next;
-			tmp->next = tmp->next->next;
-		}
-	}
-	if (to_del) {
-		sprintf(giga_buf, "server: client %d just left\n", to_del->id);
-		send_to_all(to_del->fd);
-		free(to_del);
-	}
-	FD_CLR(fd, &current);
-	close(fd);
-}
 
 int main(int ac, char **av) {
 	if (ac != 2) {
@@ -166,7 +164,7 @@ int main(int ac, char **av) {
 	struct sockaddr_in servaddr;
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(2130706433);
+	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
 	servaddr.sin_port = htons(atoi(av[1]));
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -179,38 +177,36 @@ int main(int ac, char **av) {
 
 	FD_ZERO(&current);
 	FD_SET(sockfd, &current);
-
 	while (1) {
-		read_set = write_set = current;
-		if (select(get_max_fd() + 1, &read_set, NULL, NULL, NULL) == -1)
+		readset = writeset = current;
+		if (select(get_max_fd() + 1, &readset, &writeset, NULL, NULL) == -1)
 			continue;
 		for (int fd = 0; fd <= get_max_fd(); fd++) {
-			if (FD_ISSET(fd, &read_set)) {
+			if (FD_ISSET(fd, &readset)) {
 				if (fd == sockfd) {
 					add_client();
 					break;
 				}
 				t_client *tmp = clients;
-				int ret = 1;
 				while (tmp && tmp->fd != fd)
 					tmp = tmp->next;
-				if (tmp) {
-					char buf[1024] = {0};
-					ret = recv(tmp->fd, buf, 1023, 0);
-					tmp->msg = str_join(tmp->msg, buf);
-				}
-				if (ret <= 0) {
-					rm_client(fd);
+				if (!tmp)
+					break;
+				char buf[1024] = {0};
+				int retrecv = recv(tmp->fd, buf, 1023, 0);
+				if (retrecv <= 0) {
+					remove_client(tmp->fd);
 					break;
 				}
+				tmp->buf = str_join(tmp->buf, buf);
 				char *msg = NULL;
-				while (extract_message(&(tmp->msg), &msg)) {
-					sprintf(giga_buf, "client %d: %s", tmp->id, msg);
+				while (extract_message(&tmp->buf, &msg)) {
+					sprintf(gigabuf, "client %d: %s", tmp->id, msg);
 					send_to_all(tmp->fd);
 					free(msg);
 				}
-			} /* if (FD_ISSET(fd, &read_set)) */
-		} /* for loop */
-	} /* while 1 */
+			}
+		}
+	}
 	return 0;
 }
